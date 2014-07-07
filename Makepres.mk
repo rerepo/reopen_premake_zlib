cd6fd4a94694484208ad48f26e16e761671326e6
Marcus Tang <tmc9031@gmail.com>
2014-07-07 20:27:44 +0800

    up: direct use cmd instead of var

--- Makepres.mk
+++ Makepres.mk
@@ -5,22 +5,31 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libz
 
-LOCAL_PREMAKE_CONFIG_FILE := configure
-LOCAL_PREMAKE_CONFIG_ARGS := --prefix=$(HOME)/opt/arm
-LOCAL_PREMAKE_CONFIG_ST := $(LOCAL_PATH)/configure.log
+LOCAL_PREMAKE_CONFIG_CMD := if [ ! -f $(LOCAL_PATH)/configure.log ]; then cd $(LOCAL_PATH); ./configure --prefix=$(HOME)/opt/arm; fi
 
-LOCAL_PREMAKE_MAKE_FILE := Makefile
-LOCAL_PREMAKE_MAKE_DIR := $(LOCAL_PATH)
+LOCAL_PREMAKE_MAKE_CMD := -C $(LOCAL_PATH)
 
-LOCAL_PREMAKE_MAKE_TARGET := all
 #LOCAL_PREMAKE_MAKE_ARGS := CC=$(HOME)/opt/toolschain/4.5.1/bin/arm-linux-gcc \
 LDSHARED="$(HOME)/opt/toolschain/4.5.1/bin/arm-linux-gcc -shared -Wl,-soname,libz.so.1,--version-script,zlib.map" \
 CPP="$(HOME)/opt/toolschain/4.5.1/bin/arm-linux-gcc -E" \
 AR=$(HOME)/opt/toolschain/4.5.1/bin/arm-linux-ar \
 RANLIB=$(HOME)/opt/toolschain/4.5.1/bin/arm-linux-ranlib
 
-LOCAL_PREMAKE_MAKE_CLEAN := distclean
+LOCAL_PREMAKE_MAKE_CLEAN_CMD := -C $(LOCAL_PATH) distclean
 
-LOCAL_PREMAKE_DEPEND_MODULE := makec
+LOCAL_PREMAKE_MAKE_INSTALL_CMD := \
+mkdir -p $(LOCAL_PATH)/out/lib; \
+cp -fpd $(LOCAL_PATH)/*.a $(LOCAL_PATH)/*.so* $(LOCAL_PATH)/out/lib; \
+mkdir -p $(LOCAL_PATH)/out/lib/pkgconfig; \
+cp -fp $(LOCAL_PATH)/*.pc $(LOCAL_PATH)/out/lib/pkgconfig; \
+mkdir -p $(LOCAL_PATH)/out/include; \
+cp -fp $(LOCAL_PATH)/zlib.h $(LOCAL_PATH)/zconf.h $(LOCAL_PATH)/out/include
+
+LOCAL_PREMAKE_MAKE_UNINSTALL_CMD := rm -rf $(LOCAL_PATH)/out
+
+LOCAL_PREMAKE_INSTALL_SRC := $(LOCAL_PATH)/libz.a
+LOCAL_PREMAKE_INSTALL_DST := $(addprefix $(LOCAL_PATH)/out/,$(notdir $(LOCAL_PREMAKE_INSTALL_SRC)))
+
+#LOCAL_PREMAKE_DEPEND_MODULE := makec
 
 include $(BUILD_SYSTEM)/premake.mk
